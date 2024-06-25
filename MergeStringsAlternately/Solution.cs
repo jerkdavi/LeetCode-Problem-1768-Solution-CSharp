c342c636a050a265f7be7d950db725fc289f3ac9
David Jerković <jerkdavi@gmail.com>
2024-06-25 18:39:45 +0200

    Solution class adding counter variable to use in for loop as the count to value

--- MergeStringsAlternately/Solution.cs
+++ MergeStringsAlternately/Solution.cs
@@ -9,7 +9,8 @@ namespace MergeStringsAlternately
         public static string MergeAlternately(string word1, string word2) 
         {
             string fullWord = "";
-            for (int i = 0; i < 100; i++)
+            int counter = word1.Length + word2.Length;
+            for (int i = 0; i < counter; i++)
             {
                 if (i < word1.Length) { fullWord += word1[i]; }
                 if (i < word2.Length) { fullWord += word2[i]; }
