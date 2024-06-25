563f05227256ecae5fbec23337f1fe70783e4ac1
David Jerković <jerkdavi@gmail.com>
2024-06-25 16:27:24 +0200

    Commit of LeetCode Problem 1768. Solution

---
+++ MergeStringsAlternately/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MergeStringsAlternately
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1768.
+            //string word1 = "abc";
+            //string word2 = "pqr";
+            //string word1 = "ab";
+            //string word2 = "pqrs";
+            //string word1 = "abcd";
+            //string word2 = "pq";
+            Console.Write("Word1: ");
+            string word1 = Console.ReadLine();
+            Console.Write("Word2: ");
+            string word2 = Console.ReadLine();
+
+            string fullWord = Solution.MergeAlternately(word1, word2);
+            Console.WriteLine($"Fullword: {fullWord}");
+
+            Console.WriteLine("Done processing");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MergeStringsAlternately/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MergeStringsAlternately
+{
+    public class Solution
+    {
+        public static string MergeAlternately(string word1, string word2) 
+        {
+            string fullWord = "";
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < word1.Length) { fullWord += word1[i]; }
+                if (i < word2.Length) { fullWord += word2[i]; }
+            }
+            return fullWord;
+        }
+    }
+}
